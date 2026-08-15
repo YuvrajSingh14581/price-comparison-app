dab3b29c549e3df680698f1872951f8bf5f8d2bf
Yuvraj Singh <singhyuvr04@gmail.com>
2026-08-15 16:59:18 +0530

    Add store API

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI
 
 from app.routes.products import router as products_router
+from app.routes.stores import router as stores_router
 
 
 app = FastAPI()
 
 
 app.include_router(products_router)
+app.include_router(stores_router)
 
 
 @app.get("/")
